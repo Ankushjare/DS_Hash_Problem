2fe3cacc729c8fb90677cac948dc7f78d687789e
Ankushjare <jareankush@gmail.com>
2021-11-17 07:13:16 +0530

    Data Stacture Hash Problem

---
+++ DataStructureUsingHashTables/DataStructureUsingHashTables/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DataStructureUsingHashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UC1: ");
+            UC1WordFrequency freq = new UC1WordFrequency();
+            freq.Frequency();
+
+            Console.WriteLine("\n UC2: ");
+            UC2FreqWordAndPara freq1 = new UC2FreqWordAndPara();
+            freq1.FreqPara();
+
+
+            Console.WriteLine("\n UC3: ");
+            UC3Remove rm = new UC3Remove();
+            rm.Remove();
+           
+        }
+    }
+}
